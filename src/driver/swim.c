5360e1f1e02ea3327371157e81451067adfe0a27
Joel Stein <js@funkturm.net>
2018-06-28 07:19:16 +0200

    Remove duplicate pin-configuration

--- src/driver/swim.c
+++ src/driver/swim.c
@@ -324,8 +324,6 @@ int swim_entry() {
 
   esp8266_leave_critical(state);
 
-  SET_PIN_HIGH(SWIM);
-  PIN_AS_INPUT(SWIM);
   sync_ccount(counter + duration + 24 ); // Need to wait at least 300ns (=24 cycles)
 
 #if(FIRMWARE_VERSION_MAJOR + FIRMWARE_VERSION_MINOR == 0)
